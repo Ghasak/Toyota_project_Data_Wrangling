db834cbffdca8092f77389fe47eaf164b1ba424a
Ghasak Ibrahim <mdotgh@gmail.com>
2019-07-16 11:52:53 +0900

    Working on refine variables in our dataset

---
+++ 2_Toyota_Survey_Cleaning/4_Refine_Dataset_1.py
@@ -0,0 +1,25 @@
+"""
+=========================================================
+          Refine the dataset- with configurations
+            Tue Jun. 16th 2019
+                 11:00:00,
+=========================================================
+    - The configurations of our Toyota City intersections
+        Dataset is provided through finding the best way
+        to combine all variables in our dataset
+"""
+# ==================================================#
+#           Import Libraries
+# ==================================================#
+# Load the Pandas libraries with alias 'pd'
+import pandas as pd
+import numpy as np
+import os
+from Cleaning_tools_G import *
+Current_Path = os.getcwd()
+
+
+# ==================================================#
+#              Import Dataset from Script
+#           3_Creating_Dummies_continued.py
+# ==================================================#
