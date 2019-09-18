671d76a1138af3b3d84ff8cf88b62e1b2a154acf
Ghasak Ibrahim <mdotgh@gmail.com>
2019-09-18 18:04:41 +0900

    Prepared the SpeedLimit Verfication DataSet

--- 2_Toyota_Survey_Cleaning/6_VerifyAfterEstimationSpeedLimits.py
+++ 2_Toyota_Survey_Cleaning/6_VerifyAfterEstimationSpeedLimits.py
@@ -4,6 +4,14 @@
             Wed Sep. 18th 2019
                  17:00:00,
 =========================================================
+    To Run Independently use:
+    -------------------------
+    python 2_Toyota_Survey_Cleaning/6_VerifyAfterEstimationSpeedLimits.py
+    Or using IPython:
+    %run 2_Toyota_Survey_Cleaning/6_VerifyAfterEstimationSpeedLimits.py
+
+    Working Plan:
+    -------------
     - We would like to know the speed limit crash sum by
         driver's age and verify it with the univariate
         and the multivariate model results.
@@ -20,10 +28,9 @@
 
     D- The final results should show the sum of crashes per driver's
         age and how they are refactored to our current dataframe.
-"""
 
-"""
-        To-Do List:
+    To-Do List:
+    -----------
     1- You want to create a dataset with only the accidents available
         after applying 35-rule, dummies, refined --> the Modeling Dataset
         as it called (refined.xls). This will be a dataset for both
@@ -45,24 +52,18 @@ from Cleaning_tools_G import *
 sys.path.append(os.getcwd()+"/")
 CURRENTPATH = os.getcwd()
 CRASHDATAPATH = os.path.join(CURRENTPATH+"/2_Toyota_Survey_Cleaning/SpeedLimitVerficiationData/3_Acc_ID_Final.xlsx")
-print(CRASHDATAPATH)
-
+# print(CRASHDATAPATH)
 INTERSECTIONDATAPATH = CURRENTPATH + "/2_Toyota_Survey_Cleaning/Toyota_Survey_Sheetfiles/4_Refine_Dataframe/refined_df.xlsx"
 # ==================================================#
 #           Step A Load your dataset
 # ==================================================#
 Acc_ID_Final = pd.read_excel(CRASHDATAPATH , sheet_name=0)
-
 print(Acc_ID_Final.head())
 
-
-
-# Current_Path = os.getcwd()
 # ==================================================#
 #              Import Dataset from Script
 #           3_Creating_Dummies_continued.py
 # ==================================================#
 df = pd.read_excel(INTERSECTIONDATAPATH,sheet_name='DataSet')
-
 print(df.head())
 
